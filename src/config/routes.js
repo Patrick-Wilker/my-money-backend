1f9c76079321f91042b1595ee81f342f8cde46f4
Patrick Wilker Vieira Mota <patrickvieiramota@gmail.com>
2020-10-02 12:38:44 -0300

    Add route to get the number of records

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,7 +5,7 @@ module.exports = function(server){
     const router = express.Router()
     server.use('/api', router)
 
-    //Rotas de ciclo
+    //Rotas de ciclo pagamento
     const BillingCycle = require('../api/billingCycle/billingCycleService')
     BillingCycle.register(router, '/billingCycles')
 }
